9719005798bc5b030ed25057aafe1cf1aee9045b
Robert Mollentze <robmllze@gmail.com>
2022-07-16 13:33:42 +1000

    changed tone_player_example directory

---
+++ tone_player_example/copy_and_run.zsh
@@ -0,0 +1,5 @@
+cp -R ../juce_jni/Builds/Android/lib/build/intermediates/cmake/debug_Debug/obj/arm64-v8a android/app/src/main/jniLibs/
+cp -R ../juce_jni/Builds/Android/lib/build/intermediates/cmake/debug_Debug/obj/armeabi-v7a android/app/src/main/jniLibs/
+cp -R ../juce_jni/Builds/Android/lib/build/intermediates/cmake/debug_Debug/obj/x86 android/app/src/main/jniLibs/
+cp -R ../juce_jni/Builds/Android/lib/build/intermediates/cmake/debug_Debug/obj/x86_64 android/app/src/main/jniLibs/
+flutter run
